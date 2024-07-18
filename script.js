ea54f8d876ac0f8f3e428a18de49a386d1a59011
Kyrtizan <kostua307@gmail.com>
2024-07-18 14:27:48 +0300

    second

---
+++ script.js
@@ -0,0 +1,28 @@
+var cols = 5;
+var rows = 5;
+var grid = new Array(cols);
+
+function Spot(){
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
+}
+
+function setup(){
+    createCanvas(400, 400);
+    console.log("A*");
+
+    for(var i = 0; i < cols; i++){
+        grid[i] = new Array(rows);
+    }
+
+    for(var i = 0; i < cols; i++){
+        for(var j = 0; j < cols; i++){
+            grid[i][j] = new Spot();
+        }
+    }
+}
+
+function draw(){
+    background(0);
+}
