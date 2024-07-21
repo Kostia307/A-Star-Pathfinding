a2f2b58a455d45f5be977e3241e4c9dc18f406c0
Kyrtizan <kostua307@gmail.com>
2024-07-21 14:17:31 +0300

    adding stuff

--- script.js
+++ script.js
@@ -6,15 +6,26 @@ var openSet = [];
 var closedSet = [];
 var start;
 var end;
+var w,h;
 
-function Spot(){
+function Spot(i,j){
+    this.x = i;
+    this.y = j;
     this.f = 0;
     this.g = 0;
     this.h = 0;
+
+    this.show = function(){
+        fill(255);
+        stroke(0);
+        rect(this.x * w, this.y * h, w, h);
+    }
 }
 
 function setup(){
     createCanvas(400, 400);
+    w = width / cols;
+    h = height / rows;
     console.log("A*");
 
     for(var i = 0; i < cols; i++){
@@ -23,7 +34,7 @@ function setup(){
 
     for(var i = 0; i < cols; i++){
         for(var j = 0; j < cols; i++){
-            grid[i][j] = new Spot();
+            grid[i][j] = new Spot(i,j);
         }
     }
 
@@ -36,9 +47,15 @@ function setup(){
 function draw(){
     
     if(openSet.length > 0){
-
+        //keep going
     } else {
-
+        //no solution
     }
     background(0);
+
+    for(var i = 0; i < cols; i++){
+        for(var j = 0; i < cols; j++){
+            grid[i][j].show();
+        }
+    }
 }
